bee3bddbaec2dc27adf23fc8fb3a665c0c2a2a1e
DILALOV <d-i-n-a-r@mail.ru>
2018-11-29 16:36:04 +0300

    init

---
+++ AviaTickets_ObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AviaTickets_ObjC
+//
+//  Created by Илалов Динар on 29/11/2018.
+//  Copyright © 2018 Илалов Динар. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AviaTickets_ObjC/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  AviaTickets_ObjC
+//
+//  Created by Илалов Динар on 29/11/2018.
+//  Copyright © 2018 Илалов Динар. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
